c333da1bb2a45594da91ac4bb61226729d7f7372
Ethaniel Botello <Ebotel200@west-mec.local>
2021-03-09 13:56:40 -0700

    new features

--- script.js
+++ script.js
@@ -6,4 +6,37 @@ $(document).ready(function(){
             $(".arrow-up").css({"bottom" : "-100px"})
         }
     })
+
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 650){
+            $(".skill").css({"opacity" : "10"})
+            $(".progress-bar").css({"opacity" : "10"})
+            $(".progress-bar").css({"padding-right" : "0"})
+            $(".section-title").css({"padding-left" : "0"})
+            $(".section-title").css({"opacity" : "10"})
+        } else {
+            $(".skill").css({"opacity" : "0"})
+            $(".progress-bar").css({"opacity" : "0"})
+            $(".progress-bar").css({"padding-right" : "100px"})
+            $(".section-title").css({"padding-left" : "300px"})
+            $(".section-title").css({"opacity" : "0"})
+        }
+    })
+
+    let dark = true;
+
+    function switchMode(){
+        if(dark){
+            dark = false;
+            $("body").css({"background-color" : "white"});
+            $(".switchColor").css({"color" : "black"});
+            $(".switchColor").css({"transition" : "0.3s"});
+        } else {
+            dark = true;
+            $("body").css({"background-color" : "rgb(36, 36, 36)"});
+            $(".switchColor").css({"color" : "white"});
+        }
+    }
+
+    $('#modeSwitch').on('click', switchMode);
 })
